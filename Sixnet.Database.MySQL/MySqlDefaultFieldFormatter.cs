62a5303ede81b52eadcd57bf276d5f0201f069d0
Li.DingBin <lidingbing@laoken.com>
2024-03-01 16:42:54 +0800

    'modify'

--- Sixnet.Database.MySQL/MySqlDefaultFieldFormatter.cs
+++ Sixnet.Database.MySQL/MySqlDefaultFieldFormatter.cs
@@ -6,11 +6,11 @@ namespace Sixnet.Database.MySQL
     /// <summary>
     /// Defines default field formatter for mysql
     /// </summary>
-    internal class MySqlDefaultFieldFormatter : IFieldFormatter
+    internal class MySqlDefaultFieldFormatter : ISixnetFieldFormatter
     {
-        public string Format(FieldFormatContext context)
+        public string Format(FormatFieldContext context)
         {
-            var formatOption = context.FormatOption;
+            var formatOption = context.FormatSetting;
             var formatedFieldName = context.FieldName;
             formatedFieldName = formatOption.Name switch
             {
